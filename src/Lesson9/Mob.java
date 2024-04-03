007a70d40e7919ce7b160c733002d1910e18e7aa
Ingvarr <1gorMar1n@gmail.com>
2024-04-03 17:25:51 +0300

    add hw

---
+++ src/Lesson9/Mob.java
@@ -0,0 +1,31 @@
+package Lesson9;
+//public class Mob {
+//    Person Mob = new Person();
+//    public static int counter = 0;
+//    public int Hp;
+//    public String Name;
+//    public int Damage;
+//
+//    public Mob() {
+//        counter++;
+//    }
+//
+//    public Mob(int myHP, String myName, int myDamage) {
+//        Hp = myHP;
+//        Name = myName;
+//        Damage = myDamage;
+//    }
+//    public void  infoMob(){
+//        System.out.println("Name = " + Name);
+//        System.out.println("HP = " + Hp);
+//        System.out.println("Damage = " + Damage);
+//    }
+//    public int getDamage(int Damage) {
+//        if (Damage >= 200) {
+//            System.out.println("You was dead");
+//        }
+//        return Damage;
+//    }
+//
+//}
+
